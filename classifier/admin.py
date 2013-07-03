afb9103018a19403d0e3129894377b753358df24
artscoop <artscoop93.info@gmail.com>
2013-07-03 18:02:55 +0200

    Register admin for Documents and categories
    Instead of forcing the use of management commands, allow an admin to interfere with style.

--- classifier/admin.py
+++ classifier/admin.py
@@ -1,10 +1,19 @@
 # coding: utf-8
 from django.contrib import admin
+from classifier.models import ClassifierCategory, Document
 
-
-class MailAdmin(admin.ModelAdmin):
+class ClassifierCategoryAdmin(admin.ModelAdmin):
     list_select_related = True
-    list_display = ['id','author','recipient','email','title','body','time','force']
+    list_display = ['id','categoryName', 'yes']
     list_filter = []
     readonly_fields = []
-    raw_id_fields = ['recipient', 'author']
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_select_related = True
+    list_display = ['id','corpus', 'corpusHash']
+    list_filter = []
+    readonly_fields = []
+
+admin.register(ClassifierCategory, ClassifierCategoryAdmin)
+admin.register(Document, DocumentAdmin)
